5746cd37b95666fad97a3f814ac827fb26a21bb3
mohitsingh00 <mohitsingh96210@gmail.com>
2024-04-16 12:49:47 +0530

    Book Class added

---
+++ src/main/java/com/entity/Books.java
@@ -0,0 +1,98 @@
+package com.entity;
+
+public class Books {
+
+	private int bookID;
+	private String bookName;
+	private String author;
+	private Double price;
+	private String bookCategory;
+	private String status;
+	private String photoName;
+	private String email;
+	
+	public Books(int bookID, String bookName, String author, Double price, String bookCategory, String status,
+			String photoName, String email) {
+		super();
+		this.bookID = bookID;
+		this.bookName = bookName;
+		this.author = author;
+		this.price = price;
+		this.bookCategory = bookCategory;
+		this.status = status;
+		this.photoName = photoName;
+		this.email = email;
+	}
+	
+	public int getBookID() {
+		return bookID;
+	}
+	
+	public void setBookID(int bookID) {
+		this.bookID = bookID;
+	}
+	
+	public String getBookName() {
+		return bookName;
+	}
+	
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public Double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	public String getBookCategory() {
+		return bookCategory;
+	}
+	
+	public void setBookCategory(String bookCategory) {
+		this.bookCategory = bookCategory;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	public String getPhotoName() {
+		return photoName;
+	}
+	
+	public void setPhotoName(String photoName) {
+		this.photoName = photoName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Books [bookID=" + bookID + ", bookName=" + bookName + ", author=" + author + ", price=" + price
+				+ ", bookCategory=" + bookCategory + ", status=" + status + ", photoName=" + photoName + ", email="
+				+ email + "]";
+	}
+
+}
