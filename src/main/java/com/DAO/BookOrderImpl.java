0814f207c9b8244d2df1ed871476a70d8aa1a3c0
mohitsingh00 <mohitsingh96210@gmail.com>
2024-06-01 18:50:40 +0530

    BookOrder DAO Implementation added

---
+++ src/main/java/com/DAO/BookOrderImpl.java
@@ -0,0 +1,53 @@
+package com.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+import com.entity.BookOrder;
+
+public class BookOrderImpl implements BookOrderDAO {
+
+	private Connection con;
+	
+	public BookOrderImpl(Connection con) {
+		super();
+		this.con = con;
+	}
+
+	@Override
+	public boolean saveOrder(List<BookOrder> blist)
+	{
+		boolean f = false;
+		try
+		{
+			String sql = "insert into BookOrder(order_id,username,email,address,phone,book_name,author,price,payment) values(?,?,?,?,?,?,?,?,?)";
+			con.setAutoCommit(false);
+			PreparedStatement ps = con.prepareStatement(sql);
+			for(BookOrder b:blist)
+			{
+				ps.setString(1, b.getOrderId());
+				ps.setString(2, b.getUsername());
+				ps.setString(3, b.getEmail());
+				ps.setString(4, b.getFullAddress());
+				ps.setString(5, b.getPhno());
+				ps.setString(6, b.getBookName());
+				ps.setString(7, b.getAuthor());
+				ps.setString(8, b.getPrice());
+				ps.setString(9, b.getPaymentType());
+				ps.addBatch();
+			}
+			int[] count = ps.executeBatch();
+			con.commit();
+			f = true;
+			con.setAutoCommit(true);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return f;
+	}
+
+}
